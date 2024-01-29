95565b431c57f32a0f3e48666e487ccd6e9029fe
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-01-29 10:06:37 +0100

    Create API call Login

--- FrontEnd/assets/scripts/services/api.js
+++ FrontEnd/assets/scripts/services/api.js
@@ -9,3 +9,25 @@ export async function getCategories(){
     const categories = await reponse.json();
     return categories
 }
+
+export async function loginUser(){
+    const loginForm = document.querySelector(".login-section");
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const user ={
+            email: event.target.querySelector("[name=email]").value,
+            password: event.target.querySelector("[name=password]").value,
+        };
+        const chargeUtile = JSON.stringify(user);
+
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: chargeUtile
+        })
+        .then (response => response.json())
+        .then (data => {
+            console.log(data);
+        })
+    });
+}
