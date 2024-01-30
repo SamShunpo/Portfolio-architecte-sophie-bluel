818e4bcb0f45d0ede3689766a204b0b09c156a55
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-01-30 14:07:14 +0100

    Add login logic

--- FrontEnd/assets/scripts/controllers/login.js
+++ FrontEnd/assets/scripts/controllers/login.js
@@ -1,3 +1,20 @@
 import { loginUser } from "../services/api.js";
 
-loginUser()
+const loginForm = document.querySelector(".login-section");
+loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const user = {
+        email: event.target.querySelector("[name=email]").value,
+        password: event.target.querySelector("[name=password]").value,
+    };
+    const chargeUtile = JSON.stringify(user);
+    try {
+        const loginData = await loginUser(chargeUtile)
+        window.localStorage.setItem("token",loginData.token)
+        window.location.replace("/")
+    } catch (error) {
+        const errorMessage = document.querySelector(".error_message");
+        errorMessage.innerText = error
+    }
+});
+
--- FrontEnd/assets/scripts/services/api.js
+++ FrontEnd/assets/scripts/services/api.js
@@ -1,33 +1,30 @@
-export async function getWorks(){
+/* index */
+
+export async function getWorks() {
     const reponse = await fetch(`http://localhost:5678/api/works`);
     const works = await reponse.json();
     return works
 }
 
-export async function getCategories(){
+export async function getCategories() {
     const reponse = await fetch(`http://localhost:5678/api/categories`);
     const categories = await reponse.json();
     return categories
 }
 
-export async function loginUser(){
-    const loginForm = document.querySelector(".login-section");
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const user ={
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value,
-        };
-        const chargeUtile = JSON.stringify(user);
+/*login*/
 
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: chargeUtile
-        })
-        .then (response => response.json())
-        .then (data => {
-            console.log(data);
-        })
-    });
+export async function loginUser(chargeUtile) {
+    const reponse = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile
+    })
+    if (!reponse.ok) {
+        const errorData = await reponse.json();
+        throw errorData.message;
+    } else {
+        const loginData = await reponse.json();
+        return loginData;
+    }
 }
