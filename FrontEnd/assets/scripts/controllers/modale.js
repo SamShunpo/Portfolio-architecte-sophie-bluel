d1d945009f02ed10749c3293fb78904e46daf13e
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-02-01 21:26:51 +0100

    Modal création

--- FrontEnd/assets/scripts/controllers/modale.js
+++ FrontEnd/assets/scripts/controllers/modale.js
@@ -1,5 +1,35 @@
+import { getWorks } from "../services/api.js";
+
+const works = await getWorks();
+
 const btnOpenModal = document.querySelector("#open-modal");
-const modal = document.querySelector("dialog");
+const closeModal = document.querySelector(".hide-modal");
 
 btnOpenModal.addEventListener("click", () => modal.showModal());
+
+closeModal.addEventListener("click", ()=> modal.close());
+
+
+function displayWorksModale(works) {
+    const gallery = document.querySelector(".gallery-modal");
+    gallery.innerHTML = null;
+    for (const work of works) {
+        const workElement = document.createElement("figure");
+        const imageWork = document.createElement("img");
+        const imageTrash = document.createElement("img");
+
+        imageWork.src = work.imageUrl;
+        imageWork.alt = work.title;
+
+        imageTrash.src = "./assets/icons/trash.png";
+        imageTrash.classList.add("trash");
+
+        workElement.appendChild(imageTrash);
+        workElement.appendChild(imageWork);
+        gallery.appendChild(workElement);
+    }
+}
+
+displayWorksModale(works);
+
 
