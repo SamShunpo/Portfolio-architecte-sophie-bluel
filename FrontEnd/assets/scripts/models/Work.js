dfd8db5eda88aca37aea0962ca91d66491ca87c6
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-02-19 11:42:02 +0100

    Add method createWorkElementModale

--- FrontEnd/assets/scripts/models/Work.js
+++ FrontEnd/assets/scripts/models/Work.js
@@ -1,3 +1,4 @@
+import { deleteWork } from "../services/api.js"
 export class Work {
     id
     title
@@ -5,19 +6,19 @@ export class Work {
     categoryId
     userId
 
-    constructor(workJson){
-       this.id = workJson.id;
-       this.title = workJson.title;
-       this.imageUrl = workJson.imageUrl;
-       this.categoryId = workJson.categoryId;
-       this.userId = workJson.userId;
+    constructor(workJson) {
+        this.id = workJson.id;
+        this.title = workJson.title;
+        this.imageUrl = workJson.imageUrl;
+        this.categoryId = workJson.categoryId;
+        this.userId = workJson.userId;
     }
 
-    createWorkElement(){
+    createWorkElement() {
         const workElement = document.createElement("figure");
         const imageWork = document.createElement("img");
         const figcaptionWork = document.createElement("figcaption");
-        
+
         workElement.id = `work${this.id}`;
 
         imageWork.src = this.imageUrl;
@@ -30,5 +31,41 @@ export class Work {
 
         return workElement
     }
+
+    createWorkElementModale() {
+        const workElement = document.createElement("figure");
+        const imageWork = document.createElement("img");
+        const imageTrash = document.createElement("img");
+
+        workElement.id = `modal${this.id}`;
+
+        imageWork.src = this.imageUrl;
+        imageWork.alt = this.title;
+
+        imageTrash.src = "./assets/icons/trash.svg";
+        imageTrash.id = this.id;
+        imageTrash.classList.add("trash");
+
+        imageTrash.addEventListener("click", async function (event) {
+            event.preventDefault();
+            try {
+                await deleteWork(this.id, window.localStorage.getItem("token"))
+            } catch (error) {
+                const errorMessage = document.querySelector(".error_message");
+                errorMessage.innerText = error
+            }
+            updateDisplay("modal", this.id)
+            updateDisplay("work", this.id)
+        })
+
+        workElement.appendChild(imageTrash);
+        workElement.appendChild(imageWork);
+
+        return workElement
+    }
 }
 
+function updateDisplay(name, id) {
+    const workDelete = document.getElementById(`${name}${id}`);
+    workDelete.parentNode.removeChild(workDelete);
+}
