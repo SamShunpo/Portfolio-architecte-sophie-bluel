b12d78623ff28437d5051b2aecad23562d20fee4
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-02-19 21:44:33 +0100

    form validation logic

--- FrontEnd/assets/scripts/controllers/modale.js
+++ FrontEnd/assets/scripts/controllers/modale.js
@@ -28,21 +28,25 @@ function displayWorksModale(works) {
     const gallery = document.querySelector(".gallery-modal");
     gallery.innerHTML = null;
     for (const work of works) {
-        const { workElement, imageTrash } = work.createWorkElementModale()
+        const { workElement, imageTrash} = work.createWorkElementModale()
         gallery.appendChild(workElement);
 
-        imageTrash.addEventListener("click", async function (event) {
-            event.preventDefault();
-            try {
-                await deleteWork(this.id, window.localStorage.getItem("token"))
-            } catch (error) {
-                const errorMessage = document.querySelector(".error_message");
-                errorMessage.innerText = error
-            }
-            updateDisplay("modal", this.id)
-            updateDisplay("work", this.id)
-        })
+        trashImage(work, window.localStorage.getItem("token"), imageTrash)
     }
+}
+
+function trashImage (work, token, imageTrash){
+    imageTrash.addEventListener("click", async function (event) {
+        event.preventDefault();
+        try {
+            await deleteWork(work.id, token)
+        } catch (error) {
+            const errorMessage = document.querySelector(".error_message");
+            errorMessage.innerText = error
+        }
+        updateDisplay("modal", work.id)
+        updateDisplay("work", work.id)
+    })
 }
 
 function updateDisplay(name, id) {
@@ -50,8 +54,8 @@ function updateDisplay(name, id) {
     workDelete.parentNode.removeChild(workDelete);
 }
 
-
 displayWorksModale(works);
+
 
 // Create Work change screen
 
@@ -71,25 +75,13 @@ returnArrow.addEventListener("click", () => {
     changeModal("form-works");
 })
 
-// Dropdown create work
 
-const defaultCategory = new Category({ id: 0, name: "" })
-
-function dropdownOption(categories) {
-    const selectButton = document.querySelector("#category");
-    for (const category of categories) {
-
-        selectButton.appendChild(category.getCategoryOptionElement());
-    }
-}
-
-dropdownOption([defaultCategory, ...categories])
-
-
+// FORM
 // file uploader
 
 const inputPhoto = document.getElementById('file-input');
 const inputContainer = document.querySelector(".input-file");
+let inputPhotoValidity = false
 const MAX_SIZE = 4000000;
 const ERRORS = {
     format: "Format de fichier non accepté (jpg, png)",
@@ -120,6 +112,7 @@ inputPhoto.onchange = (event) => {
     const imageContainer = document.querySelector(".picture-container");
 
     imageContainer.innerHTML = "";
+    inputContainer.style.display = "flex";
 
     resetInputContainer();
 
@@ -137,41 +130,54 @@ inputPhoto.onchange = (event) => {
                 inputPhoto.click();
             })
         };
-
+        inputPhotoValidity = true
         reader.readAsDataURL(file);
 
     } else {
         if (!file.type.match("image.*")) {
             showError(ERRORS.format);
+            inputPhotoValidity = false
         }
         if (file.size >= MAX_SIZE) {
             showError(ERRORS.size);
+            inputPhotoValidity = false
         }
     }
 };
 
 
+// Dropdown create work
 
+const defaultCategory = new Category({ id: 0, name: "" })
 
-// reset form
+function dropdownOption(categories) {
+    const selectButton = document.querySelector("#category");
+    for (const category of categories) {
 
-function resetForm(form) {
-    document.getElementById(form).reset();
+        selectButton.appendChild(category.getCategoryOptionElement());
+    }
 }
 
-function changeModal(form) {
-    const imageContainer = document.querySelector(".picture-container");
+dropdownOption([defaultCategory, ...categories]);
 
-    resetForm(form);
-    resetInputContainer()
-    deleteDiv.style.display = "flex";
-    createDiv.style.display = "none";
-    imageContainer.innerHTML = "";
-    inputContainer.style.display = "flex";
-}
+
+// check validity inputs
+
+const formValidity = document.querySelector("#form-works");
+const submitButton = document.querySelector("#submit-form");
+const inputCategoryValidity = document.querySelector("#category");
+const inputTitleValidity = document.querySelector("#title");
+
+formValidity.addEventListener("change", () => {
+    if (inputPhotoValidity === true && inputCategoryValidity.value >= 1 && inputTitleValidity.checkValidity() === true) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+});
+
 
 // create Work
-
 
 const form = document.getElementById("form-works");
 
@@ -194,13 +200,38 @@ form.addEventListener("submit", async (event) => {
 
         gallery.appendChild(work.createWorkElement());
 
-        const { workElement } = work.createWorkElementModale()
+        const { workElement, imageTrash } = work.createWorkElementModale()
         galleryModal.appendChild(workElement);
 
         dialogElement.close()
+        changeModal("form-works")
+
+        trashImage(work, window.localStorage.getItem("token"), imageTrash)
 
     } catch (error) {
-        //const errorMessage = document.querySelector("#output")
+        const errorMessage = document.querySelector("#output")
+        
     }
 })
 
+
+// reset form
+
+function resetForm(form) {
+    const submitButton = document.querySelector("#submit-form");
+
+    submitButton.disabled = true;
+    document.getElementById(form).reset();
+
+}
+
+function changeModal(form) {
+    const imageContainer = document.querySelector(".picture-container");
+
+    resetForm(form);
+    resetInputContainer()
+    deleteDiv.style.display = "flex";
+    createDiv.style.display = "none";
+    imageContainer.innerHTML = "";
+    inputContainer.style.display = "flex";
+}
